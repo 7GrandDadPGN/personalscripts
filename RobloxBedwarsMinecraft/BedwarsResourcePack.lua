78ab3033f862329674f221b666bd7e166f3c236b
7GrandDad <granddad-7@outlook.com>
2021-09-01 21:40:04 -0600

    RobloxBedwarsResourcePack

---
+++ RobloxBedwarsMinecraft/BedwarsResourcePack.lua
@@ -0,0 +1,63 @@
+repeat wait() until game:IsLoaded() == true
+repeat wait() until game.ReplicatedStorage ~= nil
+repeat wait() until game.ReplicatedStorage.Items ~= nil
+repeat wait() until game.Workspace ~= nil 
+repeat wait() until game.Workspace:FindFirstChild("Map") ~= nil
+for i,v in pairs(listfiles("bedwars")) do
+    local str = tostring(tostring(v):gsub('bedwars\\', ""):gsub(".png", ""))
+    local item = game.ReplicatedStorage.Items:FindFirstChild(str)
+    if item then
+        for i2,v2 in pairs(item:GetDescendants()) do
+            if v2:IsA("Texture") then
+                v2.Texture = getsynasset(v)
+            end
+        end
+    end
+end
+for i,v in pairs(getgc(true)) do
+    if type(v) == "table" and rawget(v, "wool_blue") and type(v["wool_blue"]) == "table" then
+        for i2,v2 in pairs(v) do
+            if isfile("bedwars/"..i2..".png") then
+                if rawget(v2, "block") and rawget(v2["block"], "greedyMesh") then
+                    if #v2["block"]["greedyMesh"]["textures"] > 1 and isfile("bedwars/"..i2.."_side_1.png") then
+                        for i3,v3 in pairs(v2["block"]["greedyMesh"]["textures"]) do
+                            v2["block"]["greedyMesh"]["textures"][i3] = getsynasset("bedwars/"..i2.."_side_"..i3..".png")
+                        end
+                    else
+                     v2["block"]["greedyMesh"]["textures"] = {
+                            [1] = getsynasset("bedwars/"..i2..".png")
+                     }
+                    end
+                    if isfile("bedwars/"..i2.."_image.png") then
+                        v2["image"] = getsynasset("bedwars/"..i2.."_image.png")
+                    end
+                else
+                    v2["image"] = getsynasset("bedwars/"..i2..".png")
+                end
+            end
+        end
+    end
+end
+for i,v in pairs(game.Workspace.Map.Blocks:GetChildren()) do
+    if isfile("bedwars/"..v.Name..".png") then
+        for i2,v2 in pairs(v:GetDescendants()) do
+            if v2:IsA("Texture") then
+                v2.Texture = getsynasset("bedwars/"..v.Name..".png")
+            end
+        end
+    end
+end
+game.Workspace.Map.Blocks.ChildAdded:connect(function(v)
+    if isfile("bedwars/"..v.Name..".png") then
+        for i2,v2 in pairs(v:GetDescendants()) do
+            if v2:IsA("Texture") then
+                v2.Texture = getsynasset("bedwars/"..v.Name..".png")
+            end
+        end
+        v.DescendantAdded:connect(function(v3)
+            if v3:IsA("Texture") then
+                v3.Texture = getsynasset("bedwars/"..v.Name..".png")
+            end
+        end)
+    end
+end)
---
+++ RobloxBedwarsMinecraft/README.txt
@@ -0,0 +1,2 @@
+Put the bedwars folder in your workspace
+Execute or put BedwarsResourcePack in your autoexec
