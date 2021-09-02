eb9f8aa81bea8ac8ee78c156a09fac6c09712c16
7GrandDad <granddad-7@outlook.com>
2021-09-01 22:46:46 -0600

    Fixed for Krnl

--- RobloxBedwarsMinecraft/BedwarsResourcePack.lua
+++ RobloxBedwarsMinecraft/BedwarsResourcePack.lua
@@ -3,13 +3,14 @@ repeat wait() until game.ReplicatedStorage ~= nil
 repeat wait() until game.ReplicatedStorage.Items ~= nil
 repeat wait() until game.Workspace ~= nil 
 repeat wait() until game.Workspace:FindFirstChild("Map") ~= nil
+local getasset = getsynasset or getcustomasset
 for i,v in pairs(listfiles("bedwars")) do
     local str = tostring(tostring(v):gsub('bedwars\\', ""):gsub(".png", ""))
     local item = game.ReplicatedStorage.Items:FindFirstChild(str)
     if item then
         for i2,v2 in pairs(item:GetDescendants()) do
             if v2:IsA("Texture") then
-                v2.Texture = getsynasset(v)
+                v2.Texture = getasset(v)
             end
         end
     end
@@ -21,18 +22,18 @@ for i,v in pairs(getgc(true)) do
                 if rawget(v2, "block") and rawget(v2["block"], "greedyMesh") then
                     if #v2["block"]["greedyMesh"]["textures"] > 1 and isfile("bedwars/"..i2.."_side_1.png") then
                         for i3,v3 in pairs(v2["block"]["greedyMesh"]["textures"]) do
-                            v2["block"]["greedyMesh"]["textures"][i3] = getsynasset("bedwars/"..i2.."_side_"..i3..".png")
+                            v2["block"]["greedyMesh"]["textures"][i3] = getasset("bedwars/"..i2.."_side_"..i3..".png")
                         end
                     else
                      v2["block"]["greedyMesh"]["textures"] = {
-                            [1] = getsynasset("bedwars/"..i2..".png")
+                            [1] = getasset("bedwars/"..i2..".png")
                      }
                     end
                     if isfile("bedwars/"..i2.."_image.png") then
-                        v2["image"] = getsynasset("bedwars/"..i2.."_image.png")
+                        v2["image"] = getasset("bedwars/"..i2.."_image.png")
                     end
                 else
-                    v2["image"] = getsynasset("bedwars/"..i2..".png")
+                    v2["image"] = getasset("bedwars/"..i2..".png")
                 end
             end
         end
@@ -42,7 +43,7 @@ for i,v in pairs(game.Workspace.Map.Blocks:GetChildren()) do
     if isfile("bedwars/"..v.Name..".png") then
         for i2,v2 in pairs(v:GetDescendants()) do
             if v2:IsA("Texture") then
-                v2.Texture = getsynasset("bedwars/"..v.Name..".png")
+                v2.Texture = getasset("bedwars/"..v.Name..".png")
             end
         end
     end
@@ -51,12 +52,12 @@ game.Workspace.Map.Blocks.ChildAdded:connect(function(v)
     if isfile("bedwars/"..v.Name..".png") then
         for i2,v2 in pairs(v:GetDescendants()) do
             if v2:IsA("Texture") then
-                v2.Texture = getsynasset("bedwars/"..v.Name..".png")
+                v2.Texture = getasset("bedwars/"..v.Name..".png")
             end
         end
         v.DescendantAdded:connect(function(v3)
             if v3:IsA("Texture") then
-                v3.Texture = getsynasset("bedwars/"..v.Name..".png")
+                v3.Texture = getasset("bedwars/"..v.Name..".png")
             end
         end)
     end
